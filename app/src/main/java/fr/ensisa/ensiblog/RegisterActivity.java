c4e85fe17ebeec58e50cf2721495db1e9ef1aee8
adrien <adrien.bocca@gmail.com>
2023-06-06 14:18:25 +0200

    Add activity_register.xml and RegisterActivity.java

--- app/src/main/java/fr/ensisa/ensiblog/RegisterActivity.java
+++ app/src/main/java/fr/ensisa/ensiblog/RegisterActivity.java
@@ -10,36 +10,28 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
     EditText edUsername, edPassword;
     TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
         edUsername = findViewById(R.id.editTextLoginUsername);
         edPassword = findViewById(R.id.editTextLoginPassword);
-        Button buttonLog = (Button) findViewById(R.id.buttonLogin);
         Button buttonReg = (Button) findViewById(R.id.buttonRegister);
-        buttonLog.setOnClickListener(new View.OnClickListener() {
+        buttonReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username = edUsername.getText().toString();
                 String password = edPassword.getText().toString();
-                if(0==0){
-                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                if (0 == 0) {
+                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
                     startActivity(intent);
-                }else{
-                    Toast.makeText(getApplicationContext(),"Wrong username/password",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong username/password", Toast.LENGTH_SHORT).show();
                 }
-            }
-        });
-        buttonReg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
-                startActivity(intent);
             }
         });
     }
