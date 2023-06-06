6696d20a242835ae67a80efeda6328f3f44ed034
combo <houmadi.combo@uha.fr>
2023-06-06 09:12:34 +0200

    biginning of acceuil page

--- app/src/main/java/fr/ensisa/ensiblog/MainActivity.java
+++ app/src/main/java/fr/ensisa/ensiblog/MainActivity.java
@@ -1,19 +1,19 @@
 package fr.ensisa.ensiblog;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import fr.ensisa.ensiblog.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MaterialToolbar topAppBar;
     private ActivityMainBinding binding;
 
     @Override
@@ -24,10 +24,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        topAppBar = findViewById(R.id.topAppBar);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.nameapp)
                 .build();
     }
 
