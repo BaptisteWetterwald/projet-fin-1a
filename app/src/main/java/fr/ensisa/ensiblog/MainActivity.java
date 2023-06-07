44cba4f93833aa0d8e1eec4ebbdf1b4765cfec45
combo <houmadi.combo@uha.fr>
2023-06-07 14:49:08 +0200

    MAJ

--- app/src/main/java/fr/ensisa/ensiblog/MainActivity.java
+++ app/src/main/java/fr/ensisa/ensiblog/MainActivity.java
@@ -1,23 +1,21 @@
 package fr.ensisa.ensiblog;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.ui.AppBarConfiguration;
+
 import fr.ensisa.ensiblog.databinding.ActivityMainBinding;
-import fr.ensisa.ensiblog.firebase.Authentication;
-import fr.ensisa.ensiblog.firebase.Database;
-import fr.ensisa.ensiblog.firebase.Table;
-import fr.ensisa.ensiblog.models.Email;
-import fr.ensisa.ensiblog.models.User;
 
 public class MainActivity extends AppCompatActivity {
        private MaterialToolbar topAppBar;
@@ -43,120 +41,6 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.nameapp)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
-
-/*        Database.getInstance().alreadyIn("Topics", new String[]{"name"}, new String[]{"ENSISA"}, new Database.AlreadyInCallback() {
-            @Override
-            public void onResult(boolean alreadyExists) {
-                if (alreadyExists) {
-                    // Do something if the topic already exists
-                    Log.i("n6a","Topic already exists");
-                } else {
-                    // Do something if the topic does not exist
-                    Log.i("n6a","Topic does not exist");
-                }
-            }
-        });
-
-        Database.getInstance().alreadyIn("Topics", new String[]{"name"}, new String[]{"1234"}, new Database.AlreadyInCallback() {
-            @Override
-            public void onResult(boolean alreadyExists) {
-                if (alreadyExists) {
-                    // Do something if the topic already exists
-                    Log.i("n6a","Topic already exists");
-                } else {
-                    // Do something if the topic does not exist
-                    Log.i("n6a","Topic does not exist");
-                }
-            }
-        });*/
-
-        String email = "test.michel@uha.fr";
-        String mdp = "2bjbkjbSBHCD%ckd@hdbzj";
-
-        Database.getInstance().add(Table.USERS.getName(), new User(new Email(email)), User.class,true);
-
-        Database.getInstance().add(Table.USERS.getName(), new User(new Email(email)), User.class,false);
-
-        /*
-        String[] fields = new String[]{"name", "defaultRole"};
-        Object[] values = new Object[]{"ENSISA", new Role(0)};
-
-        Database.getInstance().getObjects("Topics", Topic.class, fields, values).addOnSuccessListener(topics -> {
-            // Handle the retrieved topics
-            System.out.println("Topic size:" + topics.size());
-            for (Topic topic : topics) {
-                // Process each topic instance
-                Log.i("n6a", "Found " + topic.getName());
-            }
-        }).addOnFailureListener(e -> {
-            // Handle any errors that occurred during the query
-            Log.i("n6a","Error: " + e.getMessage());
-        });
-        */
-
-        /*String[] fields = new String[0];
-        Object[] values = new Object[0];
-
-        Database.getInstance().get(Table.TOPICS.getName(), Topic.class, fields, values).addOnSuccessListener(topics -> {
-            // Handle the retrieved topics
-            System.out.println("Topic size:" + topics.size());
-            for (Topic topic : topics) {
-                // Process each topic instance
-                Log.i("n6a", "Found " + topic.getName() + " with default role " + topic.getDefaultRole().getRole());
-            }
-        }).addOnFailureListener(e -> {
-            // Handle any errors that occurred during the query
-            Log.i("n6a","Error: " + e.getMessage());
-        });*/
-
-        // get topics with name "ENSISA"
-        /*Database.getInstance().get(Table.TOPICS.getName(), Topic.class, new String[]{"name"}, new String[]{"ENSISA"}).addOnSuccessListener(topics -> {
-            // Handle the retrieved topics
-            System.out.println("Topic size:" + topics.size());
-            // get the first topic
-            Topic topic = topics.get(0);
-            Topic old = new Topic(topic);
-            topic.setDefaultRole(new Role(99));
-            // update the topic
-            Database.getInstance().update(Table.TOPICS.getName(), old, topic);
-        }).addOnFailureListener(e -> {
-            // Handle any errors that occurred during the query
-            Log.i("n6a","Error: " + e.getMessage());
-        });*/
-
-        //User user = new User(new Email("baptiste.wetterwald@uha.fr"));
-        //Database.getInstance().add(Table.USERS.getName(), user, User.class);
-
-        Authentication auth = new Authentication();
-        //String email = "test.michel@uha.fr";
-        //String mdp = "2bjbkjbSBHCD%ckd@hdbzj";
-
-        /*auth.createUser(email,mdp).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                FirebaseUser user = auth.getCurrentUser();
-                String uid = user.getUid();
-                Database.getInstance().add(Table.USERS.getName(), new User(new Email(email),uid), User.class).addOnCompleteListener(task2 -> {
-                    if (task2.isSuccessful()) {
-                        Log.i("ENSIBLOG","SUCCESSFULLY CREATED USER");
-                    } else {
-                        Log.i("ENSIBLOG","FAIL TO CREATE USER");
-                    }
-                });
-            } else {
-                Log.i("ENSIBLOG","ERREUR : utilisateur existant !");
-            }
-        });*/
-        auth.signInUser(email,mdp).addOnCompleteListener(task -> {
-           if (task.isSuccessful()){
-               Log.i("ENSISABLOG","SUCCESSFULLY LOGIN");
-           } else {
-               Log.i("ENSISABLOG","FAIL TO LOGIN");
-           }
-        });
-
     }
 
 }
