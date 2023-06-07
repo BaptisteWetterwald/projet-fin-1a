9363d10b38f511a0f2cfd4ebbdbd771717ebdb6e
adrien <adrien.bocca@gmail.com>
2023-06-07 17:17:01 +0200

    add ModeratorActivity.java and activity_moderator.xml

---
+++ app/src/main/java/fr/ensisa/ensiblog/ModeratorActivity.java
@@ -0,0 +1,20 @@
+package fr.ensisa.ensiblog;
+
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.ScrollView;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class ModeratorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moderator);
+    }
+}
