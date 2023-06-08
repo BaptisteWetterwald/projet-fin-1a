3765e56a35618fd1079164aba755a551f35a98f8
BaptisteWetterwald <baptiste.wetterwald@gmail.com>
2023-06-08 11:39:27 +0200

    Setup main recycler view, displayed basic posts for Text and Image contents (videos not working for now)

--- app/src/main/java/fr/ensisa/ensiblog/models/Image.java
+++ app/src/main/java/fr/ensisa/ensiblog/models/Image.java
@@ -1,7 +1,19 @@
 package fr.ensisa.ensiblog.models;
+
+import java.io.File;
+
 public class Image extends Content {
 
     public Image(){
         // empty constructor required for firebase
     }
+
+    public Image(byte[] data) {
+        setData(data);
+    }
+
+    public Image(File file) throws Exception {
+        setData(convertObjectToBytes(file));
+    }
+
 }
--- app/src/main/java/fr/ensisa/ensiblog/models/Video.java
+++ app/src/main/java/fr/ensisa/ensiblog/models/Video.java
@@ -1,6 +1,20 @@
 package fr.ensisa.ensiblog.models;
+
+import android.net.Uri;
+
 public class Video extends Content{
+
+    private String path;
+
     public Video(){
         // empty constructor required for firebase
+    }
+
+    public Video(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
     }
 }
