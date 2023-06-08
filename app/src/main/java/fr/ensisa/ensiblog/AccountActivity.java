134ef96337d18f8875ad47287d28743e8f0fe43a
LudoE11 <ludovic.ertzer@orange.fr>
2023-06-08 17:58:27 +0200

    Added listeners on buttons

--- app/src/main/java/fr/ensisa/ensiblog/AccountActivity.java
+++ app/src/main/java/fr/ensisa/ensiblog/AccountActivity.java
@@ -1,84 +1,45 @@
 package fr.ensisa.ensiblog;
 
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class AccountActivity extends AppCompatActivity {
+public class AccountActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView textViewName;
-    private TextView textViewMail;
-    private Button buttonEditPhoto;
-    private ImageButton imageButtonEditPhoto;
     private Button buttonEditMdp;
     private Button buttonEditFunctions;
-    private TextView textViewBio;
-    private EditText editTextBio;
-    private Button buttonResiterBio;
+    private Button buttonEditBio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_management);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        textViewName = findViewById(R.id.textViewName);
-        textViewMail = findViewById(R.id.textViewMail);
-        imageButtonEditPhoto = findViewById(R.id.imageButtonEditPhoto);
+
         buttonEditMdp = findViewById(R.id.buttonEditMdp);
         buttonEditFunctions = findViewById(R.id.buttonEditFunctions);
-        textViewBio = findViewById(R.id.textViewBio);
-        editTextBio = findViewById(R.id.editTextBio);
-        buttonResiterBio = findViewById(R.id.buttonRegisterBio);
+        buttonEditBio = findViewById(R.id.buttonEditBio);
 
-        buttonEditPhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /* affiche pop-up pour choisir une nouvelle photo dans le téléphone */
-            }
-        });
+        buttonEditMdp.setOnClickListener(this);
+        buttonEditFunctions.setOnClickListener(this);
+        buttonEditBio.setOnClickListener(this);
+    }
 
-        imageButtonEditPhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /* affiche pop-up pour choisir une nouvelle photo dans le téléphone */
-            }
-        });
-
-        buttonEditMdp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /* affiche pop-up pour changer le Mdp, (editText Mdp, editText Nouveau Mdp,
-                editText Vérification Nouveau Mdp, button confirmer */
-            }
-        });
-
-        buttonEditMdp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /* affiche grosse pop-up qui affiche en liste scrolable verticalement ou dans chaque
-                * ligne il y a en text */
-            }
-        });
-
-        buttonResiterBio.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /*e ditTextBio.getText(); variable à mettre dans la base de données */
-            }
-        });
-
-
-
-
+    @Override
+    public void onClick(View view) {
+        // Gérer les clics sur les boutons
+        switch (view.getId()) {
+            case R.id.buttonEditMdp:
+                // Action à effectuer lors du clic sur le bouton "MOT DE PASSE"
+                break;
+            case R.id.buttonEditFunctions:
+                // Action à effectuer lors du clic sur le bouton "Fonction par thème"
+                break;
+            case R.id.buttonEditBio:
+                // Action à effectuer lors du clic sur le bouton "Modifier sa description"
+                break;
+        }
     }
 }
