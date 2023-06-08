cb56559f3bf09d1ab803efc06f7f93c2ce106621
Asmaa-07 <122209109+Asmaa-07@users.noreply.github.com>
2023-06-08 18:18:09 +0200

    Add Post Interface

---
+++ app/src/main/java/fr/ensisa/ensiblog/AddPostActivity.java
@@ -0,0 +1,14 @@
+package fr.ensisa.ensiblog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_post);
+    }
+}
