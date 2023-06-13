e70a70d3f5292523400107debee56638fc79dcfe
BaptisteWetterwald <baptiste.wetterwald@gmail.com>
2023-06-13 14:51:18 +0200

    Added missing View import

--- app/src/main/java/fr/ensisa/ensiblog/MainActivity.java
+++ app/src/main/java/fr/ensisa/ensiblog/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
